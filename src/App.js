e2f8de9afb6562431c7d7ca506c835914edd922e
Joonas Meriläinen <merilainenjoonas@gmail.com>
2022-01-19 07:31:20 +0200

    Add space background feature (wip)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { Time } from './components/Time';
 import { Weather } from './components/Weather';
 import { DateValue } from './components/DateValue';
+import { SpaceBackground } from './components/SpaceBackground';
 import { useState } from 'react';
 
 import { useTheme } from './services/theme/useTheme';
@@ -13,11 +14,12 @@ import OutsideClickHandler from 'react-outside-click-handler';
 function App() {
 
     const [activeModal, setActiveModal] = useState(false);
+    const [spaceMode, setSpaceMode] = useState(false);
 
     const openModal = () => setActiveModal(true);
     const closeModel = () => setActiveModal(false);
+    const isSpaceModeEnabled = false;
 
-    // eslint-disable-next-line
     const [theme, setTheme] = useTheme('night-owl');
 
     const themes = [
@@ -40,69 +42,88 @@ function App() {
     ];
 
     return (
-        <div className="flex flex-col min-h-screen transition-colors duration-700 bg-background">
-            <div className="flex justify-end p-12 text-xs text-foreground">
-            <OutsideClickHandler onOutsideClick={() => closeModel() } >
-                <div className="relative space-y-2 text-right">
-                    <AnimatePresence exitBeforeEnter initial={false}>
-                        <button
-                            className="z-50 p-4 -m-4 transition opacity-25 hover:opacity-90"
-                            onClick={() => activeModal ? closeModel() : openModal() }
-                        >
-                            {activeModal ? (
-                                <motion.span
-                                    className="block"
-                                    key="open"
-                                    animate={{ opacity: 1, scale: 1 }}
-                                    initial={{ opacity: 0, scale: 0 }}
-                                    exit={{ opacity: 0, scale: 0 }}
-                                    transition={{ duration: 0.15 }}
-                                >
-                                    <FeatherIcon icon="x" />
-                                </motion.span>
-                            ): (
-                                <motion.span
-                                    className="block"
-                                    key="closed"
-                                    animate={{ opacity: 1, scale: 1 }}
-                                    initial={{ opacity: 0, scale: 0 }}
-                                    exit={{ opacity: 0, scale: 0 }}
-                                    transition={{ duration: 0.15 }}
-                                >
-                                    <FeatherIcon icon="eye" />
-                                </motion.span>
-                            )}
-                        </button>
-                    </AnimatePresence>
-                    <AnimatePresence>
-                    {activeModal ? (
-                    <>
-                        <div className="absolute right-0 w-40">
-                            <ul className="grid gap-1 font-mono">
-                                {themes.map((value, index) => (
-                                    <motion.li
-                                        initial={{ opacity: 0, translateX: '1rem' }}
-                                        animate={{ opacity: 1, translateX: 0 }}
-                                        exit={{ opacity: 0, translateX: '1rem' }}
-                                        transition={{ duration: 0.2, delay: 0 + (index * 0.05) }}
-                                        key={value.id}
-                                    >
-                                        <button className="transition hover:scale-125" onClick={() => setTheme(value.id)}>
+        <div className="relative flex flex-col min-h-screen transition-colors duration-700 bg-background">
+            {isSpaceModeEnabled ? <SpaceBackground active={spaceMode} /> : ''}
+            <div className="flex items-center justify-end gap-4 p-12 text-xs text-foreground">
 
-                                            { theme === value.id ? (
-                                            <>
-                                                <span className="pr-1 text-primary">{value.label}</span>
-                                                <span className="text-primary">•</span>
-                                            </>
-                                            ) : <span>{value.label}</span>}
-                                        </button>
-                                    </motion.li>
-                                ))}
-                            </ul>
-                        </div>
-                    </>) : ''}
-                    </AnimatePresence>
-                </div>
+                <OutsideClickHandler onOutsideClick={() => closeModel() } >
+                    <div className="relative space-y-2 text-right">
+                        <AnimatePresence exitBeforeEnter initial={false}>
+                            <button
+                                className="z-50 p-4 -m-4 transition opacity-25 hover:opacity-90"
+                                onClick={() => activeModal ? closeModel() : openModal() }
+                            >
+                                {activeModal ? (
+                                    <motion.span
+                                        className="block"
+                                        key="open"
+                                        animate={{ opacity: 1, scale: 1 }}
+                                        initial={{ opacity: 0, scale: 0 }}
+                                        exit={{ opacity: 0, scale: 0 }}
+                                        transition={{ duration: 0.15 }}
+                                    >
+                                        <FeatherIcon icon="x" />
+                                    </motion.span>
+                                ): (
+                                    <motion.span
+                                        className="block"
+                                        key="closed"
+                                        animate={{ opacity: 1, scale: 1 }}
+                                        initial={{ opacity: 0, scale: 0 }}
+                                        exit={{ opacity: 0, scale: 0 }}
+                                        transition={{ duration: 0.15 }}
+                                    >
+                                        <FeatherIcon icon="eye" />
+                                    </motion.span>
+                                )}
+                            </button>
+                        </AnimatePresence>
+                        <AnimatePresence>
+                            {activeModal ? (
+                                <>
+                                    <div className="absolute right-0 w-40">
+                                        <ul className="grid gap-1 font-mono">
+                                            {themes.map((value, index) => (
+                                                <motion.li
+                                                    initial={{ opacity: 0, translateX: '1rem' }}
+                                                    animate={{ opacity: 1, translateX: 0 }}
+                                                    exit={{ opacity: 0, translateX: '1rem' }}
+                                                    transition={{ duration: 0.2, delay: 0 + (index * 0.05) }}
+                                                    key={value.id}
+                                                >
+                                                    <button className="transition hover:scale-125" onClick={() => setTheme(value.id)}>
+
+                                                        { theme === value.id ? (
+                                                        <>
+                                                            <span className="pr-1 text-primary">{value.label}</span>
+                                                            <span className="text-primary">•</span>
+                                                        </>
+                                                        ) : <span>{value.label}</span>}
+                                                    </button>
+                                                </motion.li>
+                                            ))}
+                                            {isSpaceModeEnabled ?
+                                                <motion.li
+                                                    initial={{ opacity: 0, translateX: '1rem' }}
+                                                    animate={{ opacity: 1, translateX: 0 }}
+                                                    exit={{ opacity: 0, translateX: '1rem' }}
+                                                    transition={{ duration: 0.2, delay: 0 + ((themes.length) * 0.05) }}
+                                                    key="space"
+                                                >
+                                                    <button
+                                                        className={`transition hover:scale-125`}
+                                                        onClick={() => setSpaceMode(! spaceMode)}
+                                                    >
+                                                    { spaceMode ? '🌎' : '🚀' }
+                                                    </button>
+                                                </motion.li>
+                                            : ''}
+                                        </ul>
+                                    </div>
+                                </>) : ''
+                            }
+                        </AnimatePresence>
+                    </div>
                 </OutsideClickHandler>
             </div>
             <div className="flex flex-col items-center justify-center flex-grow font-mono">
