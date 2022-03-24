b8c106f1ef551e0779960c419eb486b9ed384b25
Joonas Meriläinen <merilainenjoonas@gmail.com>
2022-03-24 16:46:10 +0200

    Fix rerender leaks

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useDebounce, useGeolocation, useLocalStorage } from 'react-use';
+import React, { useEffect, useState } from 'react';
+import { useGeolocation } from 'react-use';
 import FeatherIcon from 'feather-icons-react';
 
 import { formatForecast } from '../services/metno/helpers';
@@ -8,6 +8,7 @@ import { WeatherItem } from './WeatherItem';
 import useSWR from 'swr';
 
 import type { Coordinates } from '../types';
+import haversineDistance from 'haversine-distance';
 
 const fetcher = (url: string) => fetch(url).then(r => r.json())
 
@@ -22,41 +23,46 @@ const weatherWetcher = (url: string) => fetcher(url)
 
 const useLoaction = (): [Coordinates, boolean] => {
     const defaultCoordinates: Coordinates = { longitude: 0, latitude: 0 };
-    let [coords, setCoords] = useLocalStorage<Coordinates>('location', defaultCoordinates);
+    const [coords, setCoords] = useState(defaultCoordinates);
+    const [errors, setErrors] = useState(false);
 
-    const location = useGeolocation({
+    const { longitude, latitude, error } = useGeolocation({
         timeout: 5000,
         maximumAge: Number.POSITIVE_INFINITY,
     });
 
-    useDebounce(() => {
-        if (! coords) return;
-        if (location.loading) return;
+    const distance = haversineDistance(coords, {longitude: longitude ?? 0, latitude: latitude ?? 0} )
+
+    useEffect(() => {
+        if (distance < 5000) return;
 
         setCoords({
-            longitude: location.longitude ?? 0,
-            latitude: location.latitude ?? 0,
+            longitude: longitude ?? 0,
+            latitude: latitude ?? 0,
         })
-    }, 500, [location]);
+    }, [distance, longitude, latitude]);
 
-    let error = false;
+    useEffect(() => {
+        if (error) setErrors(true);
+    }, [error]);
 
-    if (location.error) {
-        error = true;
-    }
-
-    if (! coords) {
-        coords = defaultCoordinates;
-    }
-
-    return [coords, error];
+    return [coords, errors];
 }
 
-export const Weather = () => {
+const useWeather = () => {
     const [location, locationError] = useLoaction();
     const { data, error } = useSWR(location.longitude && location.latitude ? `https://api.met.no/weatherapi/locationforecast/2.0?lat=${location.latitude}&lon=${location.longitude}` : null, weatherWetcher);
 
-    if (locationError || error) {
+    return {
+        data,
+        error: error || locationError,
+    }
+}
+
+export const Weather = () => {
+    const { data, error } = useWeather();
+
+    if (error) {
         return (
             <div className="transition duration-500 delay-[5000ms] -translate-y-2 opacity-0">
                 <div className="flex items-center justify-center gap-2 text-center text-muted">
