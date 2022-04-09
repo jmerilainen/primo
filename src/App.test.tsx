b4342c6714aa323e9993e9af88c8eb1cac800623
Joonas Meriläinen <merilainenjoonas@gmail.com>
2022-04-09 13:15:42 +0300

    Remove create react app

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Primo in footer', () => {
-    render(<App />)
-    const mainElement = screen.getByText(/Primo/i);
-    expect(mainElement).toBeInTheDocument();
-});
