211c1bd2fc329a0608c30c09e00f12d226a9ebf6
Joonas Meriläinen <merilainenjoonas@gmail.com>
2022-07-18 23:18:13 +0300

    Add `eslint-plugin-import`

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,9 @@ module.exports = {
     },
     extends: [
         'eslint:recommended',
+        'plugin:import/recommended',
+        'plugin:import/react',
+        'plugin:import/typescript',
         'plugin:react/recommended',
         'plugin:react-hooks/recommended',
         'plugin:@typescript-eslint/recommended',
